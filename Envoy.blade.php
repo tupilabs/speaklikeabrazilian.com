8b87f4dcdb0c7ca6d1d62490abc76b65100d23a9
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2014-04-27 01:40:33 -0300

    Fixing deploy script

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,9 +1,7 @@
-@servers(['web' => 'www-data@66.228.62.211'])
+@servers(['web' => 'www-data@66.228.62.211 -p8422'])
 
 @task('deploy', ['on' => 'web'])
     cd /var/www/speaklikeabrazilian.com/public_html
-    git reset --hard HEAD
-    git clean -f
     git pull origin master
     composer dump-autoload
     composer update --no-dev --prefer-dist --profile -vvv
