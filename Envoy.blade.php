99346fb3722666dae053a3cddb48c8a6923abd77
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2014-06-22 22:30:23 -0300

    Fix deploy script

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -2,6 +2,8 @@
 
 @task('deploy', ['on' => 'web'])
     cd /var/www/speaklikeabrazilian.com/public_html
+    git fetch --all
+	git reset --hard origin/master
     git pull origin master
     composer dump-autoload
     composer update --no-dev --prefer-dist --profile -vvv --no-scripts
