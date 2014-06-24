f2e1e3e8ddd958d9b286dde8ca031acc2bfe39e6
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2014-06-24 15:37:50 -0300

    Rework the bootstrap responsiveness

--- public/themes/default/partials/header.twig.php
+++ public/themes/default/partials/header.twig.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-default navbar-fixed-top" role="navigation" id="nav-header">
     <div class="navbar-collapse" id="navbar-collapse-01" style="">
-    	<a href="{{ URL.action('ExpressionController@getNew') }}" class='btn btn-menu navbar-btn btn-xs'>SLBR Home</a>
+    	<a href="{{ URL.action('ExpressionController@getNew') }}" class='hidden-xs btn btn-menu navbar-btn btn-xs'>SLBR Home</a>
     	<a href="http://twitter.com/SpeakLikeABR" class='btn btn-sm navbar-btn btn-inverse btn-xs'>@SpeakLikeABR</a>
     	<a href="http://facebook.com/SpeakLikeABrazilian" class='btn btn-sm navbar-btn btn-inverse btn-xs'>Facebook Fan Page</a>
-    	<a href="{{ URL.action('ModeratorController@getModerators') }}" class='btn btn-sm btn-info navbar-btn btn-xs'>Moderators area</a>
+    	<a href="{{ URL.action('ModeratorController@getModerators') }}" class='hidden-xs btn btn-sm btn-info navbar-btn btn-xs'>Moderators area</a>
     	<a href='{{ URL.to('/en') }}' class=''><img src='/themes/default/assets/img/flags/flag_great_britain.png' width="24px" /></a>
     	<a href='{{ URL.to('/es') }}' class=''><img src='/themes/default/assets/img/flags/flag_spain.png' width="24px" /></a>
     	<a href='{{ URL.to('/it') }}' class=''><img src='/themes/default/assets/img/flags/flag_italy.png' width="24px" /></a>
@@ -14,12 +14,15 @@
 <section id='header'>
     <div class='container'>
         <div class='row'>
-            <div class='col-xs-12'>
+            <div class='col-xs-12 hidden-xs'>
 				<span class='title'>Speak Like A Brazilian<img src="{{ URL.to('/themes/default/assets/img/slbr.png') }}" class="logo" alt="Speak Like A Brazilian | Learn Portuguese" title="Speak Like A Brazilian | Learn Portuguese" /></span>
 			</div>
+            <div class='col-xs-12 visible-xs'>
+                <h5 class='title'>Speak Like A Brazilian</h5>
+            </div>
 	    </div>
 	    <div class='row'>
-	    	<div class='col-xs-6 col-xs-offset-3'>
+	    	<div class='col-md-6 col-md-offset-3 col-xs-12'>
 				{{ Form.open({'url': URL.action('SearchController@getSearch'), 'class': 'form-horizontal', 'id': 'searchFrom', 'method': 'get', 'role': 'search'}) }}
 					<div class="input-group">
 			            <input type="text" value="{{ Theme.get('q') }}" class="form-control input-sm" placeholder="Search" name="q" id="q">
