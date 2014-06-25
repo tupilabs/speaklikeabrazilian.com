40147dce7f402d0f4b7e82644004681c4da9369e
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2014-06-25 09:14:33 -0300

    Flag

--- public/themes/default/partials/header.twig.php
+++ public/themes/default/partials/header.twig.php
@@ -9,6 +9,7 @@
     	<a href='{{ URL.to('/it') }}' class=''><img src='/themes/default/assets/img/flags/flag_italy.png' width="24px" /></a>
     	<a href='{{ URL.to('/pt') }}' class=''><img src='/themes/default/assets/img/flags/flag_portugal.png' width="24px" /></a>
     	<a href='{{ URL.to('/jp') }}' class=''><img src='/themes/default/assets/img/flags/flag_japan.png' width="24px" /></a>
+        <a href='{{ URL.to('/de') }}' class=''><img src='/themes/default/assets/img/flags/flag_germany.png' width="24px" /></a>
 	</div>
 </nav>
 <section id='header'>
