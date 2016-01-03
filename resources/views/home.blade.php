158f98030973914043b62f1bb52732d5f98cec09
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-01-04 00:57:46 +1300

    Working on layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,14 +4,6 @@
 
 @section('content')
 <div class='ui container'>
-    <br />
-    <br />
-    <br />
-    <br />
     <p>This is my body content.</p>
-    <br />
-    <br />
-    <br />
-    <br />
 </div>
 @stop
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,56 +1,59 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-
-  <title>Speak Like A Brazilian</title>
-
-  <link rel="stylesheet" href="{{ URL::asset('css/slbr.css') }}">
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    
+    <title>Speak Like A Brazilian</title>
+    
+    <link rel="stylesheet" href="{{ URL::asset('css/slbr.css') }}">
 </head>
 <body>
     @section('header')
+    <div class="ui fixed center aligned small menu">
+        <div class="ui">
+            <a href="#" class="small item">Home</a>
+        </div>
+    </div>
     <div class="ui sizer vertical">
         <h1 class='ui center aligned header' id="header">Speak Like A Brazilian <img src="{{ URL::asset('images/slbr.png') }}" class="logo" alt="logo" title="logo"></h1>
     </div>
-
-    <div class="ui center aligned container">
-        <div class="ui fluid icon input">
+    
+    <div class="ui center aligned vertical basic segment">
+        <div class="ui large action input">
             <input class="prompt" placeholder="Search..." type="text">
-            <i class="search icon"></i>
+            <button class="ui button">Search</button>
         </div>
     </div>
     @show
-
+    
     @section('menu')
-    <div class="ui center aligned container">
-        <div class="ui">
-          <a class="item">
+    <div class="ui center aligned basic vertical segment" id="menu">
+        <a href="" class="item">
             New
-          </a>
-          @foreach (range('A', 'Z') as $char)
-          <a class="item">
+        </a>
+        @foreach (range('A', 'Z') as $char)
+        <a href="" class="item">
             {{ $char }}
-          </a>
-          @endforeach
-          <a class="item">
+        </a>
+        @endforeach
+        <a href="" class="item">
             0 - 9
-          </a>
-          <a class="item">
+        </a>
+        <a href="" class="item">
             Top
-          </a>
-          <a class="item">
+        </a>
+        <a href="" class="item">
             Random
-          </a>
-        </div>
+        </a>
     </div>
     @show
-
+    
     @yield('content')
-
+    
     @section('footer')
-    <div class="ui inverted vertical footer segment">
+    <div class="ui vertical footer segment" id="footer">
         <div class="ui two column grid container">
             <div class="column">
                 <h1>Speak Like A Brazilian is Open Source!</h1>
@@ -59,7 +62,7 @@
                     and distributed by great developers! The source code for this web site was
                     created by TupiLabs and open sourced via GitHub, via the MIT License.
                 </p>
-
+                
                 <p>&copy; TupiLabs &mdash; Source code licensed under MIT License</p>
             </div>
         </div>
