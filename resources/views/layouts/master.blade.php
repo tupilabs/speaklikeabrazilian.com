2bcb72fa0e4f0831dc81928396f5cfe66031a947
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-01-07 20:09:46 +1300

    Add links to top header menu

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -17,18 +17,19 @@
             <div class="center aligned column">
                 <div class="ui secondary small compact menu">
                     <a href="#" class="small item"><i class="home icon"></i> Home</a>
-                    <a href="#" class="small item"><i class="twitter square icon"></i> @SpeakLikeABR</a>
-                    <a href="#" class="small item"><i class="facebook square icon"></i> FaceBook Fan Page</a>
-                    <a href="#" class="small item"><i class="privacy icon"></i> Moderators Area</a>
+                    <a href="http://twitter.com/SpeakLikeABR" class="small item"><i class="twitter square icon"></i> @SpeakLikeABR</a>
+                    <a href="http://facebook.com/SpeakLikeABrazilian" class="small item"><i class="facebook square icon"></i> FaceBook Fan Page</a>
+                    <a href="http://speaklikeabrazilian.com/en/moderators" class="small item"><i class="privacy icon"></i> Moderators Area</a>
                     <div class="small item">
-                        <a href="#"><img width='24px' src="{{ URL::asset('images/flags/flag_great_britain.png') }}" /></a>&nbsp;&nbsp;
+                        <a href="http://speaklikeabrazilian.com/en"><img width='24px' src="{{ URL::asset('images/flags/flag_great_britain.png') }}" /></a>&nbsp;&nbsp;
+                        <a href="http://speaklikeabrazilian.com/es"><img width='24px' src="{{ URL::asset('images/flags/flag_spain.png') }}" /></a>&nbsp;&nbsp;
                     </div>
                 </div>
             </div>
         </div>
     </div>
     <div class="ui sizer vertical">
-        <h1 class='ui center aligned header' id="header-title">Speak Like A Brazilian <img src="{{ URL::asset('images/slbr.png') }}" class="logo" alt="logo" title="logo"></h1>
+        <h1 class='ui center aligned header' id="header-title">Speak Like A Brazilian <img src="{{ URL::asset('images/slbr.png') }}" class="logo" alt="Speak Like A Brazilian Logo" title="Logo"></h1>
     </div>
     
     <div class="ui center aligned vertical basic segment" id='search'>
