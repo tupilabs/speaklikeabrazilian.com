dd1c2c199915bbf655a6a4e4d2e935f64b1ffbef
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-03-16 22:37:05 +1300

    Hide mailing list field

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -2,6 +2,6 @@
 						<i class='plus icon'></i>Add Your Expression
 					</a>
 					<div class='ui divider'></div>
-					<div class='ui center aligned container'>
+					<!--<div class='ui center aligned container'>
 						<p class='faded-text'><small>Subscribe to our mailing list</small></p>
-					</div>
+					</div>-->
