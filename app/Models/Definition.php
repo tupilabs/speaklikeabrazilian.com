64d98b16d84c244a1e9118480fd351ff11188f38
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-04-02 19:38:15 +1300

    Remove unused utility methods (now in utils.php)

--- app/Models/Definition.php
+++ app/Models/Definition.php
@@ -58,38 +58,6 @@ class Definition extends Model implements Transformable
     protected $fillable = ['expression_id', 'description', 'example', 'tags', 'status', 'email', 'user_ip',
         'contributor', 'language_id'];
 
-    // --- utility methods
-
-    /**
-     * Parse the description of a definition, looking for references to other expressions enclosed by ["expression"],
-     * replacing these occurrences by links to be displayed in the UI.
-     * @return string formatted description
-     */
-    public function getFormattedDescription()
-	{
-        return $this->parseAndFormat($this->description);
-	}
-
-	public function getFormattedExample()
-	{
-        return $this->parseAndFormat($this->example);
-	}
-
-    /**
-     * Parse the value, looking for references to other expressions enclosed by ["expression"],
-     * replacing these occurrences by links to be displayed in the UI.
-     * @return string parse and formatted value
-     */
-    private function parseAndFormat($value)
-    {
-        $url = \URL::to("/");
-        $text = nl2br(urldecode($value));
-        $pattern = "/\[([^]]*)\]/i";
-        $replace = "<a href=\"{$url}/expression/define?e=$1\">$1</a>";
-        $text = preg_replace($pattern, $replace, $text);
-        return $text;
-    }
-
     // --- relationships
 
 	public function expression()
