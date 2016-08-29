f21eb41947b543d58919c0353491a0f32f7e6f87
gitolite tester <tester@example.com>
2016-08-29 21:27:31 +1200

    Add favicon header

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,10 +3,12 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta id="page_favicon" href="{{ URL::asset('images/favicon.ico') }}" rel="icon" type="image/x-icon" />
+    <link rel="shortcut icon" href="{{ URL::asset('images/favicon.ico') }}" type="image/x-icon" />
     <title>Speak Like A Brazilian</title>
-    <link rel="stylesheet" href="{{ URL::asset('css/slbr.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/slbr.css') }}" />
 </head>
 <body>
 @section('header')
--- resources/views/layouts/moderators.blade.php
+++ resources/views/layouts/moderators.blade.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta id="page_favicon" href="{{ URL::asset('images/favicon.ico') }}" rel="icon" type="image/x-icon" />
+    <link rel="shortcut icon" href="{{ URL::asset('images/favicon.ico') }}" type="image/x-icon" />
     <title>Speak Like A Brazilian</title>
     <link rel="stylesheet" href="{{ URL::asset('css/slbr.css') }}">
     <style>
