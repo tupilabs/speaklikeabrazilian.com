eb3a44dcca056eb4e9d3e82a33dd9e11eebbabd1
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2019-01-09 22:44:09 +1300

    Remove unnecessary code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,8 @@
 <template>
+    <div/>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
