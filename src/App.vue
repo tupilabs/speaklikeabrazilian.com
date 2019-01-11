2001bd8b0bafd360c157c2ca063607128af310fe
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-01-11 14:58:28 +1300

    Add another component, with data, and initial value

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <Nav />
+    <LetterNav />
     <router-view/>
   </div>
 </template>
---
+++ src/components/LetterNav.vue
@@ -0,0 +1,29 @@
+<template>
+    <b-navbar toggleable="md" type="dark" variant="info" fixed="top" sticky>
+
+        <b-navbar-toggle target="nav_collapse_letters"></b-navbar-toggle>
+
+        <b-collapse is-nav id="nav_collapse_letters">
+
+            <b-navbar-nav>
+                <b-nav-item v-for="letter in letters" :key="letter" href="#">{{ letter }}</b-nav-item>
+            </b-navbar-nav>
+
+        </b-collapse>
+    </b-navbar>
+</template>
+
+<script>
+    export default {
+        name: "LetterNav",
+        data: function() {
+            return {
+                letters: "0ABCDEFGHIJKLMNOPQRSTUVWXYZ#".split("")
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
