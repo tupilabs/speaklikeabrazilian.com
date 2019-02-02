b59a9417e3cf2aaaae584d00ed236e28c0512768
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2019-02-03 00:09:08 +1300

    Iterate definition with expression

--- docs/database2ontology.py
+++ docs/database2ontology.py
@@ -1,4 +1,4 @@
-from orm import DefinitionRepository, init_db, LanguageRepository
+from orm import DefinitionRepository, ExpressionRepository, init_db, LanguageRepository
 import logging
 
 
@@ -11,11 +11,17 @@ def main():
     db_session = init_db('mysql://root:slbr@0.0.0.0:3306/slbr')
     language_repository = LanguageRepository(db_session)
     definition_repository = DefinitionRepository(db_session)
+    expression_repository = ExpressionRepository(db_session)
     languages = language_repository.get_all()
     for language in languages:
         logging.info("Language %s", language.description)
         definitions = definition_repository.find_by_language(language.id)
         logging.info("Found %d definitions", len(definitions))
+        for definition in definitions:
+            if definition.status != '2':
+                continue
+            expression = expression_repository.find(definition.expression_id)
+            logging.info("Expression: (%s) - [%s] = [%s]", definition.status, expression.text, definition.description)
 
 
 if __name__ == '__main__':
