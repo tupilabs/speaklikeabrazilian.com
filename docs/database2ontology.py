78cf492b695a4e11d6e6564bd3a987c9943a5385
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2019-02-03 00:01:32 +1300

    Fetch definitions per language

--- docs/database2ontology.py
+++ docs/database2ontology.py
@@ -1,4 +1,4 @@
-from orm import ExpressionRepository, Expressions, init_db, LanguageRepository
+from orm import DefinitionRepository, init_db, LanguageRepository
 import logging
 
 
@@ -9,9 +9,13 @@ def main():
     global db_session
     logging.basicConfig(level=logging.INFO)
     db_session = init_db('mysql://root:slbr@0.0.0.0:3306/slbr')
-    languages = LanguageRepository(db_session).get_all()
+    language_repository = LanguageRepository(db_session)
+    definition_repository = DefinitionRepository(db_session)
+    languages = language_repository.get_all()
     for language in languages:
         logging.info("Language %s", language.description)
+        definitions = definition_repository.find_by_language(language.id)
+        logging.info("Found %d definitions", len(definitions))
 
 
 if __name__ == '__main__':
