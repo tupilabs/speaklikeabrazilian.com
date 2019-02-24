8e93004f951311e79d0bee4408fc88a8197152f9
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2019-02-24 18:12:42 +1300

    Avoid creating tables (they are already imported in Docker)

--- docs/orm.py
+++ docs/orm.py
@@ -91,5 +91,5 @@ def init_db(uri):
     db_session = scoped_session(
         sessionmaker(autocommit=False, autoflush=False, bind=engine))
     Base.query = db_session.query_property()
-    Base.metadata.create_all(bind=engine)
+    # Base.metadata.create_all(bind=engine)
     return db_session
