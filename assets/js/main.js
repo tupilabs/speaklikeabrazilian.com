38dcd0d80bb2cbcfea379ce14bff2f5c9afee5e3
kinow <kinow@users.noreply.github.com>
2024-02-02 20:53:35 +0000

    jekyll build from Action 6873d46cf5e09b61b44750127e483cd3baf56cb0

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+$(function () {
+  const $searchForm = $('#search-form')
+  $searchForm.each(function() {
+    $('input[name=query]').keydown(function(event) {
+      if (event.which === 10 || event.which === 13) {
+        this.form.submit();
+        return false;
+      }
+    });
+  });
+
+  let queryParam = location.search.split('query=')
+  if (queryParam.length > 1) {
+    queryParam = queryParam[1]
+  } else {
+    queryParam = ''
+  }
+  if ($searchForm.length) {
+    $searchForm.attr('value', queryParam)
+  }
+
+  // Algolia search
+  const search = instantsearch({
+    appId: 'FGTZFTNRCI',
+    apiKey: '89fe03b4e70cccc913c074009f696645',
+    indexName: 'slbr',
+    routing: true
+  });
+
+  // initialize SearchBox
+  search.addWidget(
+    instantsearch.widgets.searchBox({
+      container: '#search-box',
+      placeholder: 'Search for expressions'
+    })
+  );
+
+  // initialize hits widget
+  search.addWidget(
+    instantsearch.widgets.hits({
+      container: '#hits',
+      templates: {
+        empty: '<h2>No results</h2>',
+        item: hit => `
+                <li><h2><a href="${hit.permalink}">${hit.title}</a></h2></li>
+            `
+      }
+    })
+  );
+
+  // initialize pagination
+  search.addWidget(
+    instantsearch.widgets.pagination({
+      container: '#pagination',
+      maxPages: 20,
+      // default is to scroll to 'body', here we disable this behavior
+      scrollTo: false
+    })
+  );
+
+  search.start();
+});
+
+
